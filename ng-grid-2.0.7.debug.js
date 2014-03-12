df20fd84ed42ff1a89e49f5d490f2e3e08d336e7
Richard Zschech <richard.zschech@tradingscreen.com>
2014-03-12 09:38:03 -0400

    Bootstrap 3 improvements

--- ng-grid-2.0.7.debug.js
+++ ng-grid-2.0.7.debug.js
@@ -2,7 +2,7 @@
 * ng-grid JavaScript Library
 * Authors: https://github.com/angular-ui/ng-grid/blob/master/README.md 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 03/06/2014 16:22
+* Compiled At: 03/12/2014 09:36
 ***********************************************/
 (function(window, $) {
 'use strict';
@@ -2056,7 +2056,7 @@ var ngGrid = function ($scope, options, sortService, domUtilityService, $filter,
     };
 
     $scope.viewportDimHeight = function() {
-        return Math.max(0, self.rootDim.outerHeight - $scope.topPanelHeight() - $scope.footerRowHeight - 2);
+        return Math.max(0, self.rootDim.outerHeight - $scope.topPanelHeight() - $scope.footerRowHeight);
     };
     $scope.groupBy = function (col) {
         if (self.data.length < 1 || !col.groupable  || !col.field) {
